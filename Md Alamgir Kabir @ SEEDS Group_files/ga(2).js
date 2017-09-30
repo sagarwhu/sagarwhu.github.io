2a221c704a261aedee70e9131d84c26cae8c736c
Md Alamgir Kabir <sagar.whu@outlook.com>
2017-09-30 15:10:05 +0600

    Personal Webpage

---
+++ Md Alamgir Kabir @ SEEDS Group_files/ga(2).js
@@ -0,0 +1,236 @@
+							<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<!-- Debug: Bootstrap Inserted by WordPress Twitter Bootstrap CSS plugin v3.3.7-1 http://icwp.io/w/ -->
+<link rel="stylesheet" type="text/css" href="http://seedslab.org/wp-content/plugins/wordpress-bootstrap-css/resources/bootstrap-3.3.7/css/bootstrap.css" />
+<!-- / WordPress Twitter Bootstrap CSS Plugin from iControlWP. -->
+	<title>Page not found &#8211; Seeds</title>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="http://seedslab.org/xmlrpc.php">
+
+
+	<meta name='robots' content='noindex,follow' />
+<link rel='dns-prefetch' href='//fonts.googleapis.com' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel="alternate" type="application/rss+xml" title="Seeds &raquo; Feed" href="http://seedslab.org/feed/" />
+<link rel="alternate" type="application/rss+xml" title="Seeds &raquo; Comments Feed" href="http://seedslab.org/comments/feed/" />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.3\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.3\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/seedslab.org\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.8.2"}};
+			!function(a,b,c){function d(a){var b,c,d,e,f=String.fromCharCode;if(!k||!k.fillText)return!1;switch(k.clearRect(0,0,j.width,j.height),k.textBaseline="top",k.font="600 32px Arial",a){case"flag":return k.fillText(f(55356,56826,55356,56819),0,0),b=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,56826,8203,55356,56819),0,0),c=j.toDataURL(),b!==c&&(k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447),0,0),b=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447),0,0),c=j.toDataURL(),b!==c);case"emoji4":return k.fillText(f(55358,56794,8205,9794,65039),0,0),d=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55358,56794,8203,9794,65039),0,0),e=j.toDataURL(),d!==e}return!1}function e(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g,h,i,j=b.createElement("canvas"),k=j.getContext&&j.getContext("2d");for(i=Array("flag","emoji4"),c.supports={everything:!0,everythingExceptFlag:!0},h=0;h<i.length;h++)c.supports[i[h]]=d(i[h]),c.supports.everything=c.supports.everything&&c.supports[i[h]],"flag"!==i[h]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[i[h]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel='stylesheet' id='contact-form-7-css'  href='http://seedslab.org/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=4.9' type='text/css' media='all' />
+<link rel='stylesheet' id='bwg_frontend-css'  href='http://seedslab.org/wp-content/plugins/photo-gallery/css/bwg_frontend.css?ver=1.3.52' type='text/css' media='all' />
+<link rel='stylesheet' id='bwg_font-awesome-css'  href='http://seedslab.org/wp-content/plugins/photo-gallery/css/font-awesome/font-awesome.css?ver=4.6.3' type='text/css' media='all' />
+<link rel='stylesheet' id='bwg_mCustomScrollbar-css'  href='http://seedslab.org/wp-content/plugins/photo-gallery/css/jquery.mCustomScrollbar.css?ver=1.3.52' type='text/css' media='all' />
+<link rel='stylesheet' id='bwg_sumoselect-css'  href='http://seedslab.org/wp-content/plugins/photo-gallery/css/sumoselect.css?ver=3.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='athemes-headings-fonts-css'  href='//fonts.googleapis.com/css?family=Oswald%3A300%2C400%2C700&#038;ver=4.8.2' type='text/css' media='all' />
+<link rel='stylesheet' id='athemes-symbols-css'  href='http://seedslab.org/wp-content/themes/fashionistas/css/athemes-symbols.css?ver=4.8.2' type='text/css' media='all' />
+<link rel='stylesheet' id='athemes-bootstrap-css'  href='http://seedslab.org/wp-content/themes/fashionistas/css/bootstrap.min.css?ver=4.8.2' type='text/css' media='all' />
+<link rel='stylesheet' id='athemes-style-css'  href='http://seedslab.org/wp-content/themes/fashionistas/style.css?ver=4.8.2' type='text/css' media='all' />
+<style id='athemes-style-inline-css' type='text/css'>
+button, input[type="button"], input[type="reset"], input[type="submit"], #top-navigation, #top-navigation li:hover ul, #top-navigation li.sfHover ul, #main-navigation li:hover ul, #main-navigation li.sfHover ul, #main-navigation > .sf-menu > ul, .site-content [class*="navigation"] a, .widget-tab-nav li.active a, .widget-social-icons li a [class^="ico-"]:before, .site-footer { background: ; }
+#main-navigation, .entry-footer, .page-header, .author-info, .site-content [class*="navigation"] a:hover, .site-content .post-navigation, .comments-title, .widget, .widget-title, ul.widget-tab-nav, .site-extra { border-color: ; }
+.site-title a { color: ; }
+.site-description { color: ; }
+.entry-title, .entry-title a { color: ; }
+body { color: ; }
+
+</style>
+<script type='text/javascript' src='http://seedslab.org/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='http://seedslab.org/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript' src='http://seedslab.org/wp-content/plugins/photo-gallery/js/bwg_frontend.js?ver=1.3.52'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var bwg_objectsL10n = {"bwg_select_tag":"Select Tag","bwg_search":"Search"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://seedslab.org/wp-content/plugins/photo-gallery/js/jquery.sumoselect.min.js?ver=3.0.2'></script>
+<script type='text/javascript' src='http://seedslab.org/wp-content/plugins/photo-gallery/js/jquery.mobile.js?ver=1.3.52'></script>
+<script type='text/javascript' src='http://seedslab.org/wp-content/plugins/photo-gallery/js/jquery.mCustomScrollbar.concat.min.js?ver=1.3.52'></script>
+<script type='text/javascript' src='http://seedslab.org/wp-content/plugins/photo-gallery/js/jquery.fullscreen-0.4.1.js?ver=0.4.1'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var bwg_objectL10n = {"bwg_field_required":"field is required.","bwg_mail_validation":"This is not a valid email address.","bwg_search_result":"There are no images matching your search."};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://seedslab.org/wp-content/plugins/photo-gallery/js/bwg_gallery_box.js?ver=1.3.52'></script>
+<script type='text/javascript' src='http://seedslab.org/wp-content/themes/fashionistas/js/bootstrap.min.js?ver=4.8.2'></script>
+<script type='text/javascript' src='http://seedslab.org/wp-content/themes/fashionistas/js/hoverIntent.js?ver=4.8.2'></script>
+<script type='text/javascript' src='http://seedslab.org/wp-content/themes/fashionistas/js/superfish.js?ver=4.8.2'></script>
+<script type='text/javascript' src='http://seedslab.org/wp-content/themes/fashionistas/js/settings.js?ver=4.8.2'></script>
+<link rel='https://api.w.org/' href='http://seedslab.org/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://seedslab.org/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://seedslab.org/wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 4.8.2" />
+<!--[if lt IE 9]>
+<script src="http://seedslab.org/wp-content/themes/fashionistas/js/html5shiv.js"></script>
+<![endif]-->
+		<style type="text/css" id="wp-custom-css">
+			/*
+You can add your own CSS here.
+
+Click the help icon above to learn more.
+*/
+
+#main-navigation li a {
+    padding: 0 18px !important;
+}
+.site-description {
+    
+    text-transform: none !important; 
+   
+}		</style>
+	</head>
+
+<body class="error404">
+
+	<nav id="top-navigation" class="main-navigation" role="navigation">
+		<div class="clearfix container">
+		<!--	<div class="sf-menu"><ul>
+<li ><a href="http://seedslab.org/">Home</a></li><li class="page_item page-item-146"><a href="http://seedslab.org/album/">Album</a></li>
+<li class="page_item page-item-150"><a href="http://seedslab.org/seeds-contact/">Contact</a></li>
+<li class="page_item page-item-144"><a href="http://seedslab.org/link/">Link</a></li>
+<li class="page_item page-item-148"><a href="http://seedslab.org/member-recruit/">Member Recruit</a></li>
+<li class="page_item page-item-67"><a href="http://seedslab.org/our-people/">Our People</a></li>
+<li class="page_item page-item-123"><a href="http://seedslab.org/publication/">Publication</a></li>
+<li class="page_item page-item-128"><a href="http://seedslab.org/research-project/">Research Project</a></li>
+</ul></div>
+ -->
+		</div>
+	<!-- #top-navigation --></nav>
+
+	<header id="masthead" class="clearfix container site-header" role="banner">
+		<div class="site-branding">
+						
+								<div class="site-title">
+					<a href="http://seedslab.org/" title="Seeds" rel="home">
+						Seeds					</a>
+				</div>
+				<div class="site-description">SoftwarE IntElligence and Data Science Research Group</div>
+					<!-- .site-branding --></div>
+
+					<!-- #masthead --></header>
+
+	<nav id="main-navigation" class="container main-navigation" role="navigation">
+		<a href="#main-navigation" class="nav-open">Menu</a>
+		<a href="#" class="nav-close">Close</a>
+		<div class="sf-menu"><ul id="menu-main-menu" class="menu"><li id="menu-item-18" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-18"><a href="http://seedslab.org/">Home</a></li>
+<li id="menu-item-69" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-69"><a href="http://seedslab.org/our-people/">People</a></li>
+<li id="menu-item-125" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-125"><a href="http://seedslab.org/publication/">Publication</a></li>
+<li id="menu-item-130" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-130"><a href="http://seedslab.org/research-project/">Research Project</a></li>
+<li id="menu-item-154" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-154"><a href="http://seedslab.org/link/">Link</a></li>
+<li id="menu-item-152" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-152"><a href="http://seedslab.org/album/">Album</a></li>
+<li id="menu-item-155" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-155"><a href="http://seedslab.org/member-recruit/">Member Recruit</a></li>
+<li id="menu-item-153" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-153"><a href="http://seedslab.org/seeds-contact/">Contact</a></li>
+</ul></div>	<!-- #main-navigation --></nav>
+
+	<div id="main" class="site-main">
+		<div class="clearfix container">
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<section class="error-404 not-found">
+				<header class="page-header">
+					<h1 class="page-title">Oops! That page can&rsquo;t be found.</h1>
+				</header><!-- .page-header -->
+
+				<div class="page-content">
+					<p>It looks like nothing was found at this location. Maybe try one of the links below or a search?</p>
+
+					<form role="search" method="get" class="search-form" action="http://seedslab.org/">
+	<label>
+		<span class="screen-reader-text">Search for:</span>
+		<input type="search" class="search-field" placeholder="Search &hellip;" value="" name="s" title="Search for:">
+	</label>
+	<input type="submit" class="search-submit" value="Search">
+</form>
+
+							<div class="widget widget_recent_entries">		<h2 class="widgettitle">Recent Posts</h2>		<ul>
+					<li>
+				<a href="http://seedslab.org/omar-a-m-salem/">Omar A. M. Salem</a>
+						</li>
+					<li>
+				<a href="http://seedslab.org/prof-dr-touhid-bhuiyan/">Prof. Dr. Touhid Bhuiyan</a>
+						</li>
+					<li>
+				<a href="http://seedslab.org/hello-world/">Hello world!</a>
+						</li>
+				</ul>
+		</div>		
+					
+					<div class="widget widget_archive"><h2 class="widgettitle">Archives</h2><p>Try looking in the monthly archives. 🙂</p>		<label class="screen-reader-text" for="archives-dropdown--1">Archives</label>
+		<select id="archives-dropdown--1" name="archive-dropdown" onchange='document.location.href=this.options[this.selectedIndex].value;'>
+			
+			<option value="">Select Month</option>
+				<option value='http://seedslab.org/2017/08/'> August 2017 </option>
+
+		</select>
+		</div>
+					
+				</div><!-- .page-content -->
+			</section><!-- .error-404 -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<div id="widget-area-2" class="site-sidebar widget-area" role="complementary">
+	<aside id="nav_menu-3" class="widget widget_nav_menu"><h3 class="widget-title">Menu</h3><div class="menu-main-menu-container"><ul id="menu-main-menu-1" class="menu"><li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-18"><a href="http://seedslab.org/">Home</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-69"><a href="http://seedslab.org/our-people/">People</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-125"><a href="http://seedslab.org/publication/">Publication</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-130"><a href="http://seedslab.org/research-project/">Research Project</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-154"><a href="http://seedslab.org/link/">Link</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-152"><a href="http://seedslab.org/album/">Album</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-155"><a href="http://seedslab.org/member-recruit/">Member Recruit</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-153"><a href="http://seedslab.org/seeds-contact/">Contact</a></li>
+</ul></div></aside><aside id="text-3" class="widget widget_text"><h3 class="widget-title">About This Site</h3>			<div class="textwidget"><p>This may be a good place to introduce yourself and your site or include some credits.</p>
+</div>
+		</aside><aside id="text-2" class="widget widget_text"><h3 class="widget-title">Find Us</h3>			<div class="textwidget"><p><strong>Address</strong><br />
+123 Main Street<br />
+New York, NY 10001</p>
+<p><strong>Hours</strong><br />
+Monday&mdash;Friday: 9:00AM&ndash;5:00PM<br />
+Saturday &amp; Sunday: 11:00AM&ndash;3:00PM</p>
+</div>
+		</aside><!-- #widget-area-2 --></div>		</div>
+	<!-- #main --></div>
+
+	
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="clearfix container">
+				<div class="site-info">
+		&copy; 2017 Seeds. All rights reserved.	</div><!-- .site-info -->
+
+	<div class="site-credit"> Thanks to
+		<a href="http://athemes.com/theme/fashionista">Fashionista</a> for aThemes	</div><!-- .site-credit -->
+			</div>
+	<!-- #colophon --></footer>
+
+<script type='text/javascript'>
+/* <![CDATA[ */
+var wpcf7 = {"apiSettings":{"root":"http:\/\/seedslab.org\/wp-json\/contact-form-7\/v1","namespace":"contact-form-7\/v1"},"recaptcha":{"messages":{"empty":"Please verify that you are not a robot."}}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://seedslab.org/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=4.9'></script>
+<script type='text/javascript' src='http://seedslab.org/wp-includes/js/wp-embed.min.js?ver=4.8.2'></script>
+
+</body>
+</html>
